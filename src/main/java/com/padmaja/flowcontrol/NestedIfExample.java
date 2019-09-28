56d0af201ceffc3e459fb589082988eb9c369fb1
raje reddy <raje1reddy@gmail.com>
2019-09-28 09:10:04 +0100

    code for nested if example tests

--- src/main/java/com/padmaja/flowcontrol/NestedIfExample.java
+++ src/main/java/com/padmaja/flowcontrol/NestedIfExample.java
@@ -2,14 +2,21 @@ package com.padmaja.flowcontrol;
 import java.lang.String;
 
 public class NestedIfExample {
-    int score = 110;
+    public int getRankBaseOfScore(int score) {
+        if(score>200)
+            if(score<400)
+                if(score>300)
+                    return 1;
+                else
+                    return (2);
+            else
+                return (3);
+        return(6);
 
-    NestedIfExample b1 = new NestedIfExample();
+    }
 
-
+    public int getDiscount(int bill, int qty) {
+        int discount = (bill > 1000)?(qty >11)?10 : 9 : 5;
+        return discount;
+    }
 }
-
-
-
-
-
--- src/test/java/com/padmaja/flowcontrol/NestedIfExampleTest.java
+++ src/test/java/com/padmaja/flowcontrol/NestedIfExampleTest.java
@@ -4,26 +4,24 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+
 public class NestedIfExampleTest {
 
-    //@Test
-    public static void main(String args[]){
-        //NestedIfExample b1 = new NestedIfExample();
-        int score = 110;
-        if(score>200)
-            if(score<400)
-                if(score>300)
-                    System.out.println(1);
-                else
-                    System.out.println(2);
-                else
-                    System.out.println(3);
-        System.out.println(6);
-int bill = 2000;
-int qty=10;
-int discount = (bill > 1000)?(qty >11)?10 : 9 : 5;
-System.out.println(discount);
+    @Test
+    public void testGetRankBaseOfScore() {
+        NestedIfExample nestedIfExample = new NestedIfExample();
+        int result = nestedIfExample.getRankBaseOfScore(120);
+        System.out.println(result);
+        int expectedresult = 6;
+        assertEquals(expectedresult,result);
+    }
 
-
+    @Test
+    public void testGetDiscount() {
+        NestedIfExample nestedIfExample = new NestedIfExample();
+        int result = nestedIfExample.getDiscount(5,6);
+        System.out.println(result);
+        int expectedresult = 5;
+        assertEquals(expectedresult,result);
     }
 }
