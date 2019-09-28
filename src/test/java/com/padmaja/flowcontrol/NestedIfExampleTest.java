355fcf2aeb6928f0c914b26c2e1e12239f8a1a30
raje reddy <raje1reddy@gmail.com>
2019-09-28 09:19:29 +0100

    code for nested if example test cases

--- src/test/java/com/padmaja/flowcontrol/NestedIfExampleTest.java
+++ src/test/java/com/padmaja/flowcontrol/NestedIfExampleTest.java
@@ -24,4 +24,39 @@ public class NestedIfExampleTest {
         int expectedresult = 5;
         assertEquals(expectedresult,result);
     }
+
+    @Test
+    public void testGetRankBaseOfScore250() {
+        NestedIfExample nestedIfExample = new NestedIfExample();
+        int result = nestedIfExample.getRankBaseOfScore(250);
+        System.out.println(result);
+        int expectedresult = 2;
+        assertEquals(expectedresult,result);
+    }
+    @Test
+    public void testGetRankBaseOfScore350() {
+        NestedIfExample nestedIfExample = new NestedIfExample();
+        int result = nestedIfExample.getRankBaseOfScore(350);
+        System.out.println(result);
+        int expectedresult = 1;
+        assertEquals(expectedresult,result);
+    }
+
+    @Test
+    public void testGetRankBaseOfScore450() {
+        NestedIfExample nestedIfExample = new NestedIfExample();
+        int result = nestedIfExample.getRankBaseOfScore(450);
+        System.out.println(result);
+        int expectedresult = 3;
+        assertEquals(expectedresult,result);
+    }
+
+    @Test
+    public void testGetDiscount1() {
+        NestedIfExample nestedIfExample = new NestedIfExample();
+        int result = nestedIfExample.getDiscount(5000,60);
+        System.out.println(result);
+        int expectedresult = 10;
+        assertEquals(expectedresult,result);
+    }
 }
